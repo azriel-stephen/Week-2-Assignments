fb1749195b594b00a09ebb0fbb5df24639b22635
azriel-stephen <azrielstephen@gmail.com>
2023-10-03 00:08:04 +0530

    todoServer done

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,82 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
+let todos = [];
+// let todos = [
+//   {
+//     id: 1,
+//     title: "Buy groceries",
+//     completed: true,
+//     description: "I should buy groceries",
+//   },
+//   {
+//     id: 2,
+//     title: "Buy milk",
+//     completed: false,
+//     description: "I should buy milk",
+//   },
+// ];
+
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const todo = todos.find((todo) => todo.id === Number(req.params.id));
+  if (todo) res.send(todo);
+  else res.status(404).send();
+});
+
+app.post("/todos", (req, res) => {
+  // const todo = req.body;
+  if (req.body.title) {
+    const newTodo = {
+      id: Date.now(),
+      title: req.body.title,
+      description: req.body.description || null,
+      completed: req.body.completed || false,
+    };
+    todos.push(newTodo);
+    console.log(newTodo);
+    res.status(201).send(newTodo);
+  } else {
+    res.status(404).send("Title is required");
+  }
+});
+// need to be improved. now just hacking something
+app.put("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === Number(req.params.id));
+  if (todoIndex !== -1) {
+    todos[todoIndex].title = req.body.title || todos[todoIndex].title;
+    todos[todoIndex].description =
+      req.body.description || todos[todoIndex].description;
+    todos[todoIndex].completed =
+      req.body.completed || todos[todoIndex].completed;
+    res.status(200).json(todos[todoIndex]);
+  } else {
+    res.status(404).json({ ...req.body, id: "Not Found" });
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === Number(req.params.id));
+  if (todoIndex !== -1) {
+    todos.splice(todoIndex, 1);
+    res.status(200).json(todos[todoIndex]);
+  } else {
+    res.status(404).json({ ...req.body, id: "Not Found" });
+  }
+});
+
+// app.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
 
 module.exports = app;
