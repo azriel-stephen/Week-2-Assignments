615a636e54e30c20ed1ba87abcb9ba4a45645359
azriel-stephen <azrielstephen@gmail.com>
2023-10-03 12:10:45 +0530

    file server done but something wrong with tests - route working but test failed 1 of 5

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -16,10 +16,33 @@
 
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
+app.get("/files", (req, res) => {
+  fs.readdir(path.join(__dirname, "/files"), (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to retrieve filesss" });
+    } else res.json(files);
+  });
+});
+
+app.get("/file/:filename", (req, res) => {
+  const filepath = path.join(__dirname, "./files/", req.params.filename);
+
+  fs.readFile(filepath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+
+// just for testing purposes
+// app. listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
 
 module.exports = app;
